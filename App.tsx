a622511fca5bfe359094b86f4f6695757b097439
DexxLabs <ranbeersingh0520@gmail.com>
2024-06-10 01:17:15 +0530

    Added State Function and TouchableOpacity

--- App.tsx
+++ App.tsx
@@ -1,118 +1,71 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity, View, useColorScheme } from 'react-native'
+import React, { useState } from 'react'
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export default function App() {
+  const mode = useColorScheme() === 'dark'
+  const [colorChange, changeColor] = useState("#000000")
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  const generateColor = () => {
+    const HexValues = "0123456789abcdef"
+    let colorValue = "#"
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+    for (let i = 0; i < 6; i++) {
+      const element = HexValues[(Math.floor(Math.random()*16))];
+        colorValue+=element
+      
+    }
+    changeColor(colorValue);
+  }
+  
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <>
+      <StatusBar backgroundColor={colorChange}/>
+        <View style={[styles.container, {backgroundColor: colorChange}]}>
+          <View style={mode?styles.buttonContainer : styles.buttonContainerLight }>
+            <TouchableOpacity onPress={generateColor}>
+          <Text style={mode?styles.headingText:styles.headingTextLight}>Color Switch</Text>
+          </TouchableOpacity>
+          </View>
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+        
+    </>
+  )
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container:{
+    flex:1 ,
+    
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  headingTextLight:{
+    padding:20,
+    textAlign: 'center',
+    fontFamily: 'Montserrat-SemiBold',
+    color: '#fff',
+    fontSize: 16,
+    backgroundColor: '#000',
+    borderRadius: 30
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+  headingText:{
+    padding:20,
+    textAlign: 'center',
+    fontFamily: 'Montserrat-SemiBold',
+    color: '#000',
+    fontSize: 16,
+    backgroundColor: '#fff',
+    borderRadius: 30
 
-export default App;
+  },
+  buttonContainer:{
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center'
+    
+  },
+  buttonContainerLight:{
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
